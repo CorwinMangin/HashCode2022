be827b0e381649b7a35418c4864dd3079d9f66e9
Chloé Lere <chloe.lere@epitech.eu>
2022-02-24 13:31:23 -0500

    edit main

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@
 #include "Project.hpp"
 #include "Person.hpp"
 #include <list>
-
+#include <array>
 int main()
 {
     std::map<Project, std::list<Person>> actualProject;
-    return 33;
+    std::list<Project> projects;
+    std::list<Person> persons;
+    return 0;
 }
